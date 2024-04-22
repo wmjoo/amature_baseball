a71815b22a45b0b3ba25e2c471e3a77fe15c387e
wmjoo <38180565+wmjoo@users.noreply.github.com>
2024-04-23 00:28:17 +0900

    Update records.py
    탭 표시 변경, 전체 데이터 밑에 링크 url 표시

--- records.py
+++ records.py
@@ -73,15 +73,17 @@ df_p_concat = pd.concat([data_p[0], data_p[1]], axis = 0).reset_index(drop=True)
 df_p_concat = df_p_concat.drop(['순위'], axis = 1)
 #t.write(df_p_concat)
 
-tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["[전체타자]", "[전체투수]", "타자:규정타석이상", "타자:규정타석미만", "투수:규정이닝이상", "투수:규정이닝미달"])
+tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["[전체타자]", "[전체투수]", "타자:규정이상", "타자:규정미만", "투수:규정이상", "투수:규정미달"])
 
 with tab1:
     st.subheader('전체 타자 : {}'.format(team_name))
     st.dataframe(df_b_concat)
+    st.write(DATA_URL_B)
 
 with tab2:
     st.subheader('전체 투수 : {}'.format(team_name))
     st.dataframe(df_p_concat) 
+    st.write(DATA_URL_P)
 
 with tab3:
    st.subheader('규정타석 이상 : {}'.format(team_name))
