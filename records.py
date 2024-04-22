14ead5a42de421f52b8484487ac0271b773ce4d7
wmjoo <38180565+wmjoo@users.noreply.github.com>
2024-04-22 23:56:19 +0900

    Update records.py

--- records.py
+++ records.py
@@ -4,17 +4,41 @@ import pandas as pd
 import numpy as np
 import requests
 
-st.title('GAMEONE Batter DATA')
+st.title('성남(토요)리그 DATA')
 
 # DATE_COLUMN = 'date/time'
-DATA_URL = "http://www.gameone.kr/club/info/ranking/hitter?club_idx=7984"
+team_id_dict = {
+    "코메츠 호시탐탐": 7984,
+    "Big Hits": 36636,
+    "FA Members": 13621,
+    "RedStorm": 17375,
+    "unknown`s": 33848,
+    "그냥하자": 10318,
+    "기드온스": 27811,
+    "다이아몬스터": 39783,
+    "데빌베어스(Devil Bears)": 19135,
+    "라이노즈": 41236,
+    "미파스": 19757,
+    "분당스타즈": 34402,
+    "블루레이커즈": 22924,
+    "성시야구선교단": 29105,
+    "와사비": 14207,
+}
+
+team_name = st.selectbox(
+    '팀 선택',
+    (team_id_dict.keys()))
+
+# st.write('You selected TEAM:', option)
+
+team_id = team_id_dict[team_name]
+
+DATA_URL_B = "http://www.gameone.kr/club/info/ranking/hitter?club_idx={}".format(team_id)
+DATA_URL_P = "http://www.gameone.kr/club/info/ranking/pitcher?club_idx={}".format(team_id)
 
 # @st.cache_data
-def load_data(url = DATA_URL):
-    # data = pd.read_csv(DATA_URL, nrows=nrows)
-    # lowercase = lambda x: str(x).lower()
-    # data.rename(lowercase, axis='columns', inplace=True)
-    # data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+def load_data(url):
+    # st.write(url)
     # 웹 페이지에서 데이터 가져오기
     response = requests.get(url)
     tables = pd.read_html(response.text)
@@ -22,33 +46,48 @@ def load_data(url = DATA_URL):
     # 데이터 확인
     for i, table in enumerate(tables):
         if i == 0:
-            above_minPA = table
-            # print(above_minPA.head(2))
-            above_minPA = above_minPA.rename(columns={'게임수': '경기'}, inplace=False)
-            # print(above_minPA.head(2))
+            above_min = table
+            if 'hitter' in url :
+                above_min = above_min.rename(columns={'게임수': '경기'}, inplace=False)
+            else:
+                above_min = above_min.rename(columns={'게임수': '경기수'}, inplace=False)                
         else :
-            below_minPA = table
-    return above_minPA, below_minPA
+            below_min = table
+    return above_min, below_min
 
-# Create a text element and let the reader know the data is loading.
-# data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data()
+data_b = load_data(DATA_URL_B)
+data_p = load_data(DATA_URL_P)
+# st.write(data_p)
 
-df_concat = pd.concat([data[0], data[1]], axis = 0).reset_index(drop=True)
-df_concat = df_concat.drop(['순위'], axis = 1)
+df_b_concat = pd.concat([data_b[0], data_b[1]], axis = 0).reset_index(drop=True)
+df_b_concat = df_b_concat.drop(['순위'], axis = 1)
 
-# Notify the reader that the data was successfully loaded.
-# data_load_state.text('Loading data...done!')
-# data_load_state.text("Done! (using st.cache_data)")
+df_p_concat = pd.concat([data_p[0], data_p[1]], axis = 0).reset_index(drop=True)
+df_p_concat = df_p_concat.drop(['순위'], axis = 1)
+#t.write(df_p_concat)
 
-st.subheader('Raw entire data')
-st.write(df_concat)
+tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["[전체타자]", "[전체투수]", "타자:규정타석이상", "타자:규정타석미만", "투수:규정이닝이상", "투수:규정이닝미달"])
 
-if st.checkbox('Show raw data(Above PA)'):
-    st.subheader('규정 타석 이상')
-    st.write(data[0])
+with tab1:
+    st.subheader('전체 타자 : {}'.format(team_name))
+    st.write(df_b_concat)
 
-if st.checkbox('Show raw data(Below PA)'):
-    st.subheader('규정 타석 미만')
-    st.write(data[1])
+with tab2:
+    st.subheader('전체 투수 : {}'.format(team_name))
+    st.write(df_p_concat) 
+
+with tab3:
+   st.subheader('규정타석 이상 : {}'.format(team_name))
+   st.write(data_b[0])
+
+with tab4:
+   st.subheader('규정타석 미만 : {}'.format(team_name))
+   st.write(data_b[1]) 
+
+with tab5:
+    st.subheader('규정이닝 이상 : {}'.format(team_name))
+    st.write(data_p[0])   
+
+with tab6:
+    st.subheader('규정이닝 미달 : {}'.format(team_name))
+    st.write(data_p[1])       
