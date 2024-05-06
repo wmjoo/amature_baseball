a4ec058e7050d96c4184c3fe76fc8c5b9e16de73
wmjoo <38180565+wmjoo@users.noreply.github.com>
2024-05-06 11:27:06 +0900

    Update records.py

--- records.py
+++ records.py
@@ -172,7 +172,7 @@ with tab4:
         grouped_df['ERA'] = ((grouped_df['ER'] / grouped_df['IP']) * 9).round(3)
     
     # 이닝당 삼진/볼넷/피안타 계산 (예제로 삼진(K), 볼넷(BB), 피안타(HA) 컬럼 필요)
-    if 'SO' in df_pitcher.columns and 'BB' in df_pitcher.columns and 'HA' in df.columns:
+    if 'SO' in df_pitcher.columns and 'BB' in df_pitcher.columns and 'HA' in df_pitcher.columns:
         grouped_df['SO/IP'] = (grouped_df['SO'] / grouped_df['IP']).round(2)
         grouped_df['BB/IP'] = (grouped_df['BB'] / grouped_df['IP']).round(2)
         grouped_df['H/IP'] = (grouped_df['HA'] / grouped_df['IP']).round(2)
