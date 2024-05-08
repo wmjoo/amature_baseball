3ff0d5fd8d8fee7fd3df6d9a3e5f48851321c2f9
wmjoo <38180565+wmjoo@users.noreply.github.com>
2024-05-08 19:25:12 +0900

    #378 등 .sort_values('Team').T

--- records.py
+++ records.py
@@ -370,27 +370,26 @@ with tab_sn_viz:
                                     color_discrete_sequence=px.colors.qualitative.D3, #px.colors.sequential.Plasma_r,
                                     template=template_input, title=f'수비력 : {team1} vs {team2}')
             ########################
-
             ## Chart AND Dataframe display Area
             tab_sn_vs_col2_1, tab_sn_vs_col2_2 = st.columns(2)   
             with tab_sn_vs_col2_1:            # 차트 보기 [Hitter]
                 if not team_all: #if team_selection_rader == 'VS':        
                     st.dataframe(pd.concat([hitter_grpby.loc[hitter_grpby.Team == team1, selected_cols_h], 
-                                        hitter_grpby.loc[hitter_grpby.Team == team2, selected_cols_h]], axis = 0).T)   
+                                        hitter_grpby.loc[hitter_grpby.Team == team2, selected_cols_h]], axis = 0).sort_values('Team').T)   
                     # st.dataframe(pd.concat([filtered_data_h.loc[filtered_data_h.Team == team1, selected_cols_h], 
                     #                     filtered_data_h.loc[filtered_data_h.Team == team2, selected_cols_h]], axis = 0))        
                 else :
-                    st.dataframe(hitter_grpby[selected_cols_h].T)
+                    st.dataframe(hitter_grpby[selected_cols_h].sort_values('Team').T)
                     # st.dataframe(filtered_data_h[selected_cols_h])
                 st.plotly_chart(fig_h, use_container_width=True)
             with tab_sn_vs_col2_2:             # 차트 보기 [Pitcher]
                 if not team_all: #if team_selection_rader == 'VS':                
                     st.dataframe(pd.concat([pitcher_grpby.loc[pitcher_grpby.Team == team1, selected_cols_p], 
-                                        pitcher_grpby.loc[pitcher_grpby.Team == team2, selected_cols_p]], axis = 0).T)   
+                                        pitcher_grpby.loc[pitcher_grpby.Team == team2, selected_cols_p]], axis = 0).sort_values('Team').T)   
                     # st.dataframe(pd.concat([filtered_data_p.loc[filtered_data_p.Team == team1, selected_cols_p], 
                     #                     filtered_data_p.loc[filtered_data_p.Team == team2, selected_cols_p]], axis = 0))     
                 else :
-                    st.dataframe(pitcher_grpby[selected_cols_p].T)                    
+                    st.dataframe(pitcher_grpby[selected_cols_p].sort_values('Team').T)                    
                     # st.dataframe(filtered_data_p[selected_cols_p])
                 st.plotly_chart(fig_p, use_container_width=True)
 
