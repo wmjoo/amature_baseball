c05e323adf8e80b614c2b8477967a7bb460d2396
wmjoo <38180565+wmjoo@users.noreply.github.com>
2024-05-08 17:24:58 +0900

    .

--- records.py
+++ records.py
@@ -326,22 +326,22 @@ with tab_sn_viz:
             # 선택된 데이터셋에 따라 데이터 프레임 설정
             selected_cols_h = ['Team', 'BA', 'OBP', 'OPS', 'BB', 'SO', 'SB']
             selected_cols_p = ['Team', 'ERA', 'WHIP', 'H/IP', 'BB/IP', 'SO/IP']        
-            hitter_grpby_rank_scaled = hitter_grpby_rank.copy()
+            hitter_grpby_scaled = hitter_grpby.copy()
             scaler_h = MinMaxScaler()             # 스케일러 초기화
-            hitter_grpby_rank_scaled[hitter_grpby_rank_scaled.columns[1:]] = scaler_h.fit_transform(hitter_grpby_rank_scaled.iloc[:, 1:]) # 첫 번째 열 'Team'을 제외하고 스케일링
-            
-            pitcher_grpby_rank_scaled = pitcher_grpby_rank.copy()
+            hitter_grpby_scaled[hitter_grpby_scaled.columns[1:]] = scaler_h.fit_transform(hitter_grpby_scaled.iloc[:, 1:]) # 첫 번째 열 'Team'을 제외하고 스케일링
+
+            pitcher_grpby_scaled = pitcher_grpby_rank.copy()
             scaler_p = MinMaxScaler()             # 스케일러 초기화
-            pitcher_grpby_rank_scaled[pitcher_grpby_rank_scaled.columns[1:]] = scaler_p.fit_transform(pitcher_grpby_rank_scaled.iloc[:, 1:]) # 첫 번째 열 'Team'을 제외하고 스케일링
+            pitcher_grpby_scaled[pitcher_grpby_scaled.columns[1:]] = scaler_p.fit_transform(pitcher_grpby_scaled.iloc[:, 1:]) # 첫 번째 열 'Team'을 제외하고 스케일링
 
             if team_selection_rader == '전체':
-                filtered_data_h = hitter_grpby_rank_scaled
+                filtered_data_h = hitter_grpby_scaled
                 radar_data_h = filtered_data_h[selected_cols_h].melt(id_vars=['Team'], var_name='Stat', value_name='Value')
                 fig_h = px.line_polar(radar_data_h, r='Value', theta='Stat', color='Team', line_close=True,
                                     color_discrete_sequence=px.colors.qualitative.D3, #px.colors.sequential.Plasma_r,
                                     template=template_input, title=f'공격력')   
 
-                filtered_data_p = pitcher_grpby_rank_scaled
+                filtered_data_p = pitcher_grpby_scaled
                 radar_data_p = filtered_data_p[selected_cols_p].melt(id_vars=['Team'], var_name='Stat', value_name='Value')
                 fig_p = px.line_polar(radar_data_p, r='Value', theta='Stat', color='Team', line_close=True,
                                     color_discrete_sequence=px.colors.qualitative.D3, #px.colors.sequential.Plasma_r,
@@ -349,7 +349,7 @@ with tab_sn_viz:
 
             else: # team_selection_rader == 'VS' : 2개팀을 비교할 경우
                 # 선택된 팀 데이터 필터링
-                filtered_data_h = hitter_grpby_rank_scaled[hitter_grpby_rank_scaled['Team'].isin([team1, team2])].copy()
+                filtered_data_h = hitter_grpby_scaled[hitter_grpby_scaled['Team'].isin([team1, team2])].copy()
                 # 레이더 차트 데이터 준비
                 radar_data_h = filtered_data_h[selected_cols_h].melt(id_vars=['Team'], var_name='Stat', value_name='Value')
                 # 레이더 차트 생성
@@ -357,7 +357,7 @@ with tab_sn_viz:
                                     color_discrete_sequence=px.colors.qualitative.D3, #px.colors.sequential.Plasma_r,
                                     template=template_input, title=f'공격력 : {team1} vs {team2}')
                 # 선택된 팀 데이터 필터링
-                filtered_data_p = pitcher_grpby_rank_scaled[pitcher_grpby_rank_scaled['Team'].isin([team1, team2])].copy()
+                filtered_data_p = pitcher_grpby_scaled[pitcher_grpby_scaled['Team'].isin([team1, team2])].copy()
                 # 레이더 차트 데이터 준비
                 radar_data_p = filtered_data_p[selected_cols_p].melt(id_vars=['Team'], var_name='Stat', value_name='Value')
                 # 레이더 차트 생성
@@ -365,6 +365,7 @@ with tab_sn_viz:
                                     color_discrete_sequence=px.colors.qualitative.D3, #px.colors.sequential.Plasma_r,
                                     template=template_input, title=f'수비력 : {team1} vs {team2}')
 
+            ## Chart AND Dataframe display Area
             tab_sn_vs_col2_1, tab_sn_vs_col2_2 = st.columns(2)   
             with tab_sn_vs_col2_1:            # 차트 보기 [Hitter]
                 if team_selection_rader == 'VS':        
