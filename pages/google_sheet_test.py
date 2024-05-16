933a58cbd65d49be3b579e970e886ff0ef502ea3
wmjoo <38180565+wmjoo@users.noreply.github.com>
2024-05-16 13:25:26 +0900

    load yn btn ADD

--- pages/google_sheet_test.py
+++ pages/google_sheet_test.py
@@ -1,8 +1,11 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-# Create a connection object.
-conn = st.connection("gsheets", type=GSheetsConnection)
-df = conn.read()
-st.subheader('Google Sheets')
-st.dataframe(df, use_container_width = True, hide_index = True)
+load_yn = st.button('Load Data')
+
+if load_yn :
+    # Create a connection object.
+    conn = st.connection("gsheets", type=GSheetsConnection)
+    df = conn.read()
+    st.subheader('Google Sheets')
+    st.dataframe(df, use_container_width = True, hide_index = True)
