cf425040425054ceb690d4e8f9cf8f1aa92cfdf2
Woomin Joo <38180565+wmjoo@users.noreply.github.com>
2024-08-04 07:38:57 +0900

    Update records.py

--- records.py
+++ records.py
@@ -573,6 +573,8 @@ with tab_schd:
     st.write(df_schd2)
     first_called = df_schd2.선공점수.str.contains('콜드승')
     second_called = df_schd2.후공점수.str.contains('콜드승')
+    st.write(df_schd2.선공점수.str.replace('콜드승 ', ''))
+    st.write(df_schd2.선공점수.str.replace('콜드승 ', '').replace('기권승 ', ''))
     df_schd2.선공점수 = df_schd2.선공점수.str.replace('콜드승 ', '').replace('기권승 ', '').astype('int')  #.replace('', 0).astype('int')
     df_schd2.후공점수 = df_schd2.후공점수.str.replace('콜드승 ', '').replace('기권승 ', '').fillna(0).astype('int')
     df_schd2['Result'] = ''
