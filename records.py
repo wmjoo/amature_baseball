a3ec94b0f2c74996a19da7f6664d51a45c984a9c
wmjoo <38180565+wmjoo@users.noreply.github.com>
2025-03-25 14:05:37 +0900

    .

--- records.py
+++ records.py
@@ -446,8 +446,8 @@ with tab_sn_teamwise:
                 {}
                 </div>
             """.format(
-                "\n".join([f"{k}: {v}, " for k, v in df_h_meandict.items()]),
-                "\n".join([f"{k}: {v}, " for k, v in df_h_mediandict.items()])
+                "\n".join([f"{k}: {v}, " for k, v in df_h_meandict.items()])[:-1],
+                "\n".join([f"{k}: {v}, " for k, v in df_h_mediandict.items()][:-1])
             )
             # st.write(box_stylesetting)
             st.markdown(h_box_stylesetting, unsafe_allow_html=True)            
