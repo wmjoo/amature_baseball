feb7773159df7fcdd354ab895da145810e15e86e
wmjoo <38180565+wmjoo@users.noreply.github.com>
2025-03-27 09:04:41 +0900

    .

--- records_wings.py
+++ records_wings.py
@@ -715,7 +715,7 @@ with tab_sn_teams: # 팀 기록 탭
         hitter_heatmap_df = hitter_grpby_rank.copy()
         # 팀명 컬럼을 영어 팀명으로 매핑하여 'team_eng' 컬럼 생성
         hitter_heatmap_df['team_eng'] = hitter_heatmap_df['Team'].map(team_name_dict)
-
+        # 팀명을 기준으로 우리팀이 맨위에 오도록 설정
         hitter_heatmap_df = hitter_heatmap_df.sort_values(by='Team')        # 1. Team 명 기준 오름차순 정렬
         target = hitter_heatmap_df[hitter_heatmap_df['Team'].str.contains('SKCC')]  # 2. 특정 문자열이 있는 행 필터링
         others = hitter_heatmap_df[~hitter_heatmap_df['Team'].str.contains('SKCC')]         # 3. 나머지 행 필터링
@@ -730,6 +730,12 @@ with tab_sn_teams: # 팀 기록 탭
         pitcher_heatmap_df = pitcher_grpby_rank.copy()
         # 팀명을 영어로 매핑하여 'team_eng' 컬럼 생성
         pitcher_heatmap_df['team_eng'] = pitcher_heatmap_df['Team'].map(team_name_dict)
+        # 팀명을 기준으로 우리팀이 맨위에 오도록 설정
+        pitcher_heatmap_df = pitcher_heatmap_df.sort_values(by='Team')        # 1. Team 명 기준 오름차순 정렬
+        target = pitcher_heatmap_df[pitcher_heatmap_df['Team'].str.contains('SKCC')]  # 2. 특정 문자열이 있는 행 필터링
+        others = pitcher_heatmap_df[~pitcher_heatmap_df['Team'].str.contains('SKCC')]         # 3. 나머지 행 필터링
+        pitcher_heatmap_df = pd.concat([target, others], ignore_index=True)  # 4. 두 데이터프레임을 위에서 아래로 concat
+
         # 기존 'Team' 컬럼 제거 후 'team_eng'를 인덱스로 설정
         pitcher_heatmap_df = pitcher_heatmap_df.drop('Team', axis=1).copy()
         pitcher_heatmap_df.set_index('team_eng', inplace=True)
