2fa01aaa7aabf7735866f23ef07fa9dc06e6da79
wmjoo <38180565+wmjoo@users.noreply.github.com>
2025-03-27 09:00:35 +0900

    .

--- records_wings.py
+++ records_wings.py
@@ -721,8 +721,8 @@ with tab_sn_teams: # 팀 기록 탭
 
         # 1. score 기준 오름차순 정렬
         hitter_heatmap_df = hitter_heatmap_df.sort_values(by='team_eng')
-        target = hitter_heatmap_df[hitter_heatmap_df['name'].str.contains('SKCC')]  # 2. 특정 문자열이 있는 행 필터링
-        others = hitter_heatmap_df[~hitter_heatmap_df['name'].str.contains('SKCC')]         # 3. 나머지 행 필터링
+        target = hitter_heatmap_df[hitter_heatmap_df['team_eng'].str.contains('SKCC')]  # 2. 특정 문자열이 있는 행 필터링
+        others = hitter_heatmap_df[~hitter_heatmap_df['team_eng'].str.contains('SKCC')]         # 3. 나머지 행 필터링
         hitter_heatmap_df = pd.concat([target, others], ignore_index=True)  # 4. 두 데이터프레임을 위에서 아래로 concat
     
         # 수비지표 히트맵용 데이터프레임 준비
